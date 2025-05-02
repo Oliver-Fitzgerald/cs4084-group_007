d47218133e6eee2efe51b1dd5545c7f3e6e358ac
Gary3c <150540901+Gary3c@users.noreply.github.com>
2025-05-02 13:51:55 +0100

    merge conflict

--- app/src/main/java/com/college/cs4048_group_007/entities/SaleItem.java
+++ app/src/main/java/com/college/cs4048_group_007/entities/SaleItem.java
@@ -30,7 +30,7 @@ public class SaleItem {
 
     @ColumnInfo(name = "description")
     public String description;
-<<<<<<< Updated upstream
+
     @ColumnInfo(name = "timer")
     public long timer;
 
@@ -42,15 +42,13 @@ public class SaleItem {
         this.name = name;
         this.description = description;
         this.timer = timer;
-=======
-    public SaleItem(){
-
     }
+
     public SaleItem(int poiId, float price, String name, String desc){
         this.poiId = poiId;
         this.price = price;
         this.name = name;
         this.description = desc;
->>>>>>> Stashed changes
+
     }
 }
